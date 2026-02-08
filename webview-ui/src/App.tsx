6810819a12382070d62dc2cdc88b0696f24daff6
Nada-Hany <nadaaayman993@gmail.com>
2026-02-08 22:49:57 +0200

    extension base

---
+++ webview-ui/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+  <div>
+    <h1>Mindtrace is Running!</h1>
+    <p>If you see this, React is working.</p>
+  </div>
+);
+}
+
+export default App
