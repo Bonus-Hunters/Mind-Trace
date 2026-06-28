0a122a11271933b0dcbf1d00fc24bf2346803676
Nada Hany <nadaaayman993@gmail.com>
2026-06-28 05:14:37 +0300

    ingest notes of mind trace in db

--- backend/core/database/models.py
+++ backend/core/database/models.py
@@ -1,20 +1,20 @@
 from datetime import datetime
-from typing import List, Optional, Dict, Any
-from sqlalchemy import (
-    String,
-    ForeignKey,
-    DateTime,
-    ARRAY,
-    Text,
-    ForeignKeyConstraint,
-    UniqueConstraint,
-    Float,  
-    Identity,
-)
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from typing import Any, Dict, List, Optional
+
 from pgvector.sqlalchemy import Vector
-from utils.constants import EMBEDDING_SIZE
+from sqlalchemy import (
+    ARRAY,
+    DateTime,
+    Float,
+    ForeignKey,
+    Identity,
+    String,
+    Text,
+    UniqueConstraint,
+)
 from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from utils.constants import EMBEDDING_SIZE
 
 
 class Base(DeclarativeBase):
